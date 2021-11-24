7512286fd553d699fc3f6f88290ed38e33d32249
HalaAlbaqawi <hala.hammed.dmm.k1@tuwaiq.edu.sa>
2021-11-24 15:39:33 +0300

    hala's updates

---
+++ app/src/main/java/com/example/geogalleryproject/view/DialogFragment.kt
@@ -0,0 +1,22 @@
+package com.example.geogalleryproject.view
+
+import android.app.DialogFragment
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.geogalleryproject.R
+
+  class DialogFragment :   DialogFragment () {
+
+
+      override fun onCreateView(
+          inflater: LayoutInflater, container: ViewGroup?,
+          savedInstanceState: Bundle?
+      ): View? {
+          // Inflate the layout for this fragment
+          return inflater.inflate(R.layout.fragment_dialog, container, false)
+      }
+
+  }
