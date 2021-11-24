5128fb69b8f9edaf5459bc0e13c920db8c7b0d62
fatimahmayouf <ftmmtf1995@gmail.com>
2021-11-24 17:00:56 +0300

    "current location + fix running time errors"

--- app/src/main/java/com/example/geogalleryproject/database/GeoGalleryDatabase.kt
+++ app/src/main/java/com/example/geogalleryproject/database/GeoGalleryDatabase.kt
@@ -3,9 +3,10 @@ package com.example.geogalleryproject.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.geogalleryproject.model.GeoGalleryModel
+import com.example.geogalleryproject.model.Photo
 
 
-@Database(entities = [GeoGalleryModel::class], version = 1)
+@Database(entities = [Photo::class], version = 1)
 abstract class GeoGalleryDatabase : RoomDatabase() {
 
     abstract fun geoGalleryDao(): GeoGalleryDao
