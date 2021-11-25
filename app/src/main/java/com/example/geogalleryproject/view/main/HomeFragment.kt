cedc2323b1520b6a5c2fb60e888fdf6ad1a7886a
HalaAlbaqawi <hala.hammed.dmm.k1@tuwaiq.edu.sa>
2021-11-25 11:39:54 +0300

    FRAGMENTS UPDATES @Belal

--- app/src/main/java/com/example/geogalleryproject/view/main/HomeFragment.kt
+++ app/src/main/java/com/example/geogalleryproject/view/main/HomeFragment.kt
@@ -6,16 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.geogalleryproject.R
+import com.example.geogalleryproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false)
+        return binding.root
     }
+
 
 }
