1d6d5fea6ea7d95846e55bda6134250e3cf56f1d
Belal Al-Rammah <91452283+iBelal995@users.noreply.github.com>
2021-11-25 14:21:18 +0300

    "theme change"

--- app/src/main/java/com/example/geogalleryproject/view/Splash.kt
+++ app/src/main/java/com/example/geogalleryproject/view/Splash.kt
@@ -2,11 +2,10 @@ package com.example.geogalleryproject.view
 
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.example.geogalleryproject.R
-import com.example.geogalleryproject.view.main.MainActivity
 
 lateinit var handler: Handler
 
