cdf5f04e82a836c317655ac3aef7b676c24b9335
Belal Al-Rammah <91452283+iBelal995@users.noreply.github.com>
2021-11-29 14:33:12 +0300

    update

--- app/src/main/java/com/example/geogalleryproject/view/main/HomeFragment.kt
+++ app/src/main/java/com/example/geogalleryproject/view/main/HomeFragment.kt
@@ -1,10 +1,9 @@
 package com.example.geogalleryproject.view.main
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.geogalleryproject.R
@@ -14,6 +13,10 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,17 +26,14 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return binding.root
 
-
-
-
-
-
-
-
-
-
-
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        requireActivity().menuInflater.inflate(R.menu.actionbarmenu, menu)
 
+        val searchItem = menu.findItem(R.id.app_bar_search)
+        val location = menu.findItem(R.id.app_bar_location)
+        val searchView = searchItem.actionView as SearchView
+
+        }
 
 }
