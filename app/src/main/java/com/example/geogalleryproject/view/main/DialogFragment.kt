6138ad219184cf0d4a2ae8548fea75b272d4c6a6
Belal Al-Rammah <91452283+iBelal995@users.noreply.github.com>
2021-11-30 22:07:59 +0300

    update dialog

--- app/src/main/java/com/example/geogalleryproject/view/main/DialogFragment.kt
+++ app/src/main/java/com/example/geogalleryproject/view/main/DialogFragment.kt
@@ -3,13 +3,39 @@ package com.example.geogalleryproject.view.main
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.geogalleryproject.R
+import com.example.geogalleryproject.databinding.FragmentDialogBinding
 
 class DialogFragment : DialogFragment () {
 
+      private lateinit var binding: FragmentDialogBinding
+      override fun onCreateView(
+          inflater: LayoutInflater, container: ViewGroup?,
+          savedInstanceState: Bundle?
+      ): View? {
+          binding = FragmentDialogBinding.inflate(inflater, container, false)
+          // Inflate the layout for this fragment
+          return binding.root
+
+      }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addFav.setOnClickListener {
+            Toast.makeText(context, "hi", Toast.LENGTH_SHORT).show()
+        }
+
+    }
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
         AlertDialog.Builder(requireContext())
             .setView(R.layout.fragment_dialog)
             .create()
+
+
+
   }
